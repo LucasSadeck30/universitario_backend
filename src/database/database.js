f5b38f020899cba409be3b0f3d35f0e6d0bbf219
LucasSadeck30 <lucasdevsadeck@gmail.com>
2026-02-23 19:00:01 -0300

    volta configuracão padrão

--- src/database/database.js
+++ src/database/database.js
@@ -1,30 +1,14 @@
 require('dotenv').config({ override: false });
 
-console.log("DATABASE_URL:", process.env.DATABASE_URL); 
+const { PrismaClient } = require('@prisma/client');
 
-const {PrismaClient} = require('@prisma/client');
-const { PrismaPg} = require('@prisma/adapter-pg')
-const { Pool } = require('pg');
-
-// Usar connectionString que já está funcionando!
-const pool = new Pool({
-    connectionString: process.env.DATABASE_URL,
-    ssl: {
-        rejectUnauthorized: false
-    }
-});
-
-const adapter = new PrismaPg(pool);
-
-const prisma = new PrismaClient({
-    adapter: adapter
-});
+const prisma = new PrismaClient();
 
 async function testar_conexao() {
-    prisma.$connect().then(()=>{
+    prisma.$connect().then(() => {
         console.log("Conexão realizada com sucesso");
-    }).catch((err)=>{
-        console.log("Erro ao conectar com o banco de dados da faculdade:" + err)
+    }).catch((err) => {
+        console.log("Erro ao conectar:" + err)
     })
 }
 
